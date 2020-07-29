1993c05ce568e7d79dc5a99724ea9a119c2f2501
chao <zhangc257@163.com>
2020-07-29 09:13:16 +0000

    init css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,21 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App') - Laravel Test</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">Help</li>
+        <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
