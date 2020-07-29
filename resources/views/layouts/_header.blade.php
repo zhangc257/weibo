5ca54159ebccc65a912e0caabbe4c33fe1dcc902
chao <zhangc257@163.com>
2020-07-29 10:35:38 +0000

    add _header and _footer

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a class="nav-link" href="/help">Help</li>
+      <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+    </ul>
+  </div>
+</nav>
