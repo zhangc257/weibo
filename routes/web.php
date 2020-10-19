2970d124bae72736d778b81c325d3c6dc82a8ab2
chao <zhangc257@163.com>
2020-10-19 10:10:07 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
